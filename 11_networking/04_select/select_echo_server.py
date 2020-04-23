87fed2296f42857cf952629198f5752ee962903b
ariesduanmu <aries.duanmu@gmail.com>
2020-04-23 18:04:24 +0800

    select

--- 11_networking/04_select/select_echo_server.py
+++ 11_networking/04_select/select_echo_server.py
@@ -2,7 +2,7 @@
 # @Author: Li Qin
 # @Date:   2020-04-22 16:54:15
 # @Last Modified by:   Li Qin
-# @Last Modified time: 2020-04-22 17:07:14
+# @Last Modified time: 2020-04-23 17:09:48
 import select
 import socket
 import sys
@@ -17,3 +17,60 @@ server.bind(server_address)
 
 server.listen(5)
 
+inputs = [server]
+
+outputs = []
+
+message_queues = {}
+
+while inputs:
+    print('waiting for the next event', file=sys.stderr)
+    # The first is a list of the objects to be checked for incoming data to be read, 
+    # the second contains objects that will receive outgoing data when there is room in their buffer, 
+    # and the third includes those objects that may have an error (usually a combination of the input and output channel objects). 
+    readable, writable, exceptional = select.select(inputs, outputs, inputs)
+    for s in readable:
+        if s is server:
+            connection, client_address = s.accept()
+            print(f'  connection from {client_address}', file=sys.stderr)
+
+            connection.setblocking(0)
+            inputs.append(connection)
+
+            message_queues[connection] = queue.Queue()
+        else:
+            data = s.recv(1024)
+            if data:
+                print(f'  received {data} from {s.getpeername()}', file=sys.stderr)
+
+                if s not in outputs:
+                    outputs.append(s)
+
+            else:
+                print(f' closing {client_address}', file=sys.stderr)
+                if s in outputs:
+                    outputs.remove(s)
+
+                inputs.remove(s)
+                s.close()
+
+                del message_queues[s]
+
+    for s in writable:
+        try:
+            next_msg = message_queues[s].get_nowait()
+        except queue.Empty:
+            print(f'  {s.getpeername()} queue empty', file=sys.stderr)
+            outputs.remove(s)
+        else:
+            print(f'  sending {next_msg} to {s.getpeername()}', file=sys.stderr)
+            s.send(next_msg)
+
+    for s in exceptional:
+        print(f'exception condition on {server.getpeername()}', file=sys.stderr)
+
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+        del message_queues[s]
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # python3_standard_library
+
 python3 standard library examples
+
+* [01 text](https://github.com/ariesduanmu/python3_standard_library/tree/master/01_text)
+* [02_data_structures](https://github.com/ariesduanmu/python3_standard_library/tree/master/02_data_structures)
+* [03_algorithms](https://github.com/ariesduanmu/python3_standard_library/tree/master/03_algorithms)
+* [04_dates_and_times](https://github.com/ariesduanmu/python3_standard_library/tree/master/04_dates_and_times)
+* [05_mathematics](https://github.com/ariesduanmu/python3_standard_library/tree/master/05_mathematics)
+* [06_the_file_system](https://github.com/ariesduanmu/python3_standard_library/tree/master/06_the_file_system)
+* [07_data_persistence_and_exchange](https://github.com/ariesduanmu/python3_standard_library/tree/master/07_data_persistence_and_exchange)
+* [08_data_compression_and_archiving](https://github.com/ariesduanmu/python3_standard_library/tree/master/08_data_compression_and_archiving)
+* [09_cryptography](https://github.com/ariesduanmu/python3_standard_library/tree/master/09_cryptography)
+* [10_processes_and_threads](https://github.com/ariesduanmu/python3_standard_library/tree/master/10_processes_and_threads)
+* [11_networking](https://github.com/ariesduanmu/python3_standard_library/tree/master/11_networking)
+* [12_the_internet](https://github.com/ariesduanmu/python3_standard_library/tree/master/12_the_internet)
+* [13_email](https://github.com/ariesduanmu/python3_standard_library/tree/master/13_email)
+* [14_application_building_blocks](https://github.com/ariesduanmu/python3_standard_library/tree/master/14_application_building_blocks)
+* [15_internationalization](https://github.com/ariesduanmu/python3_standard_library/tree/master/15_internationalization)
+* [16_developer_tools](https://github.com/ariesduanmu/python3_standard_library/tree/master/16_developer_tools)
+* [17_runtime_features](https://github.com/ariesduanmu/python3_standard_library/tree/master/17_runtime_features)
+* [18_language_tools](https://github.com/ariesduanmu/python3_standard_library/tree/master/18_language_tools)
+* [19_modules_and_packages](https://github.com/ariesduanmu/python3_standard_library/tree/master/19_modules_and_packages)
